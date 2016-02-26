7b2dadb4a771bcd78b56dc32f887c474d1534125
andersonmcook <andersonmcook@gmail.com>
2016-02-26 14:40:16 -0600

    user route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const userRoutes = require('./user/user.route')
 
 app.set('view engine', 'jade');
 
@@ -19,35 +20,11 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
 
+// Use body parser
 app.use(bodyParser.urlencoded({extended: false}));
 
-// GET home is a login?
-app.get('/', (req, res) => {
-  res.render('form');
-});
-
-// POST login info
-app.post('/', (req, res) => {
-  res.send('you have logged in');
-  //res.redirect(`/form/${res.uid}`) ?
-});
-
-// GET form to input day/steps/weight
-app.get('/form/:id', (req, res) => {
-  //req.params.id
-  res.send(`Enter your weight and steps for the day, ${req.params.id}.`);
-});
-
-// POST the form
-app.post('/form', (req, res) => {
-  res.send('you have posted some info');
-  //res.redirect(`/chart/${uid of some sort}`);
-});
-
-// GET chart
-app.get('/chart/:id', (req, res) => {
-  res.send('a chart');
-});
+// use user.route.js
+app.use(userRoutes);
 
 // listen
 app.listen(PORT, () => {
---
+++ user/user.route.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+// GET home is a login?
+router.get('/', (req, res) => {
+  // res.render('form');
+  res.send('login');
+});
+
+// POST login info
+router.post('/', (req, res) => {
+  res.send('you have logged in');
+  //res.redirect(`/form/${res.uid}`) ?
+});
+
+// GET form to input day/steps/weight
+router.get('/form/:id', (req, res) => {
+  //req.params.id
+  res.render('form');
+});
+
+// POST the form
+router.post('/form', (req, res) => {
+  res.send('you have posted some info');
+  //res.redirect(`/chart/${uid of some sort}`);
+});
+
+// GET chart
+router.get('/chart/:id', (req, res) => {
+  res.send('a chart');
+});
+
+module.exports = router;
