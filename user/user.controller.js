023b633207e92244c1739f3b1213dd83f1f2d5d8
andersonmcook <andersonmcook@gmail.com>
2016-02-27 23:03:31 -0600

    spacing

--- user/user.controller.js
+++ user/user.controller.js
@@ -13,11 +13,13 @@ module.exports = {
       y: [10, 15, 13, 17],
       type: "scatter"
     };
+
     const trace2 = {
       x: [1, 2, 3, 4],
       y: [16, 5, 11, 9],
       type: "scatter"
     };
+
     const data = [trace1, trace2];
     const graphOptions = {filename: "basic-line", fileopt: "overwrite"};
     plotly.plot(data, graphOptions, function (err, msg) {
